17b338fc655da2e1c39da040e66bdacaad530d73
dewebkiller <dewebkiller@gmail.com>
2025-10-21 22:47:44 +0545

    section features and how it works

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
 <head>
     <title>Lodbod</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Lodbod">
     <meta name="keywords" content="Lodbod">
     <meta name="author" content="dewebkiller">
@@ -12,8 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/reset.css" rel="stylesheet">
     <link href="css/layout.css" rel="stylesheet">
-    <link href="css/owl.carousel.css" rel="stylesheet">
-    <link href="css/owl.theme.default.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
     <link href="css/animate.css" rel="stylesheet">
     <link href="css/hover.css" rel="stylesheet">
--- index.php
+++ index.php
@@ -37,18 +37,78 @@
         </div>
     </div>
 </section>
-<section class="section-padding quote-block">
+<section class="section-padding section-features bg-light">
     <div class="container">
         <div class="row">
-            <div class="col-lg-8">
-                <div class="quote">
-                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
+            <div class="col-lg-4 col-12">
+                <div class="content d-flex flex-column align-items-center text-center">
+                    <div class="icon-round">
+                        <img src="images/icon-vehicle.svg" alt="Streamline dispatches">
+                    </div>
+                    <h3>Streamline dispatches</h3>
+                    <p> No more calls or chaos. Post a load in minutes and get matched with verified truckers.</p>
                 </div>
             </div>
-            <div class="col-lg-4" data-aos="fade-up">
-                <div class="quote-btn">
-                    <a href="#" class="btn btn-success btn-lg">Request a quote</a>
+
+            <div class="col-lg-4 col-12">
+                <div class="content d-flex flex-column align-items-center text-center">
+                    <div class="icon-round">
+                        <img src="images/icon-target.svg" alt="Streamline dispatches">
+                    </div>
+                    <h3>Track every trip</h3>
+                    <p> Live updates from loading dock to  delivery point—no guesswork.</p>
                 </div>
+            </div>
+            <div class="col-lg-4 col-12">
+                <div class="content d-flex flex-column align-items-center text-center">
+                    <div class="icon-round">
+                        <img src="images/icon-downtime.svg" alt="Streamline dispatches">
+                    </div>
+                    <h3>Reduce downtime</h3>
+                    <p> Get faster turnaround and fewer delays with a platform designed for consistency.</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<section class="section-padding section-how-it-works">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-12">
+                <div class="content-how-it-works d-flex flex-column">
+                    <h2><span class="color-primary">Get matched </span> with <br /> a truck in minutes</h2>
+                    <ul class="list-unstyled steps-list d-flex flex-column gap-3">
+                        <li class="step-item">
+                            <a href="#" class="step-link active-step">
+                                <h3 class="step-title">1. Create an account</h3>
+                                <p class="step-description">Sign up or install the app for free.</p>
+                            </a>
+                        </li>
+
+                        <li class="step-item">
+                            <a href="#" class="step-link">
+                                <h3 class="step-title">2. Post your load</h3>
+                                <p class="step-description">Enter pickup, drop, and load details.</p>
+                            </a>
+                        </li>
+
+                        <li class="step-item">
+                            <a href="#" class="step-link">
+                                <h3 class="step-title">3. Get matched & track</h3>
+                                <p class="step-description">Get connected with a reliable trucker. Monitor the entire trip live.</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-12">
+                <div class="image-how-it-works d-flex flex-column">
+                    <img src="images/Image-howitworks.png
+                    " alt="How it works" class="img-fluid" />
+                </div>
+
             </div>
         </div>
     </div>
