7b2df7c5c6080a97e6b811ea1228b10a1ed02b5d
dewebkiller <dewebkiller@gmail.com>
2025-10-22 13:43:10 +0545

    blog section

--- index.php
+++ index.php
@@ -113,7 +113,7 @@
         </div>
     </div>
 </section>
-<section class="content-wrapper section-padding section-testimonial">
+<section class="content-wrapper section-padding-2x section-testimonial">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
@@ -225,6 +225,8 @@
     </div>
 </section>
 
+
+
 <section class="section-padding-2x section-benefits bg-black color-white">
     <div class="container">
         <div class="row">
@@ -261,6 +263,59 @@
                     <h3>Bid & deliver</h3>
                     <p> Place bids, authorize drivers, manage cancellations, and shift vehicles as needed.</p>
                 </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<section class="section-padding-2x section-blog">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+            <h2>Read on to know more about our products</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-12 blog-card-wrapper d-flex flex-column">
+                <div class="blog-content">
+                    <div class="blog-card-thumbnail">
+                        <img src="images/logistics.png" alt="How to make your logistics easy">
+                    </div>
+                    <div class="blog-card-content d-flex flex-column">
+                    <h3>How to make your logistics easy</h3>
+                    <p> Explore our blog for insider tips and stories from the world of logistics.</p>
+                    </div>
+                </div>
+
+                <a href="#" class="btn-blog">Read More </a>  
+            </div>
+
+            <div class="col-lg-4 col-12 blog-card-wrapper d-flex flex-column">
+                <div class="blog-content">
+                    <div class="blog-card-thumbnail">
+                        <img src="images/logistics.png" alt="How to make your logistics easy">
+                    </div>
+                    <div class="blog-card-content d-flex flex-column">
+                    <h3>How to make your logistics easy</h3>
+                    <p> Explore our blog for insider tips and stories from the world of logistics. Explore our blog for insider tips and stories from the world of logistics.</p>
+                    </div>
+                </div>
+
+                <a href="#" class="btn-blog">Read More </a>  
+            </div>
+
+            <div class="col-lg-4 col-12 blog-card-wrapper d-flex flex-column">
+                <div class="blog-content">
+                    <div class="blog-card-thumbnail">
+                        <img src="images/no-image.png" alt="How to make your logistics easy">
+                    </div>
+                    <div class="blog-card-content d-flex flex-column">
+                    <h3>How to make your logistics easy</h3>
+                    <p> Explore our blog for insider tips and stories from the world of logistics.</p>
+                    </div>
+                </div>
+
+                <a href="#" class="btn-blog">Read More </a>  
             </div>
 
         </div>
