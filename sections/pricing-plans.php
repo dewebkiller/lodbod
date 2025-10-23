e6312473417cefa9a1e7373c4512d2379f19dd98
dewebkiller <dewebkiller@gmail.com>
2025-10-23 14:06:39 +0545

    diamond pricing card

--- sections/pricing-plans.php
+++ sections/pricing-plans.php
@@ -6,77 +6,108 @@
         <p class="p-0">Choose a plan that’s right for you.</p>
       </div>
     </div>
+    <div class="row d-grid">
+      <div class="pricing-card bg-white color-black d-flex flex-column">
+        <div class="pricing-top">
+          <div class="pricing-title">
+            <h4>Basic</h4>
+            <p> No minimum commitment Pause or cancel anytime</p>
+          </div>
+          <div class="pricing-package">
+            <h3>Free</h3>
+            <p> Lifetime</p>
+            <div class="gradient-divider"></div>
+          </div>
+          <div class="pricing-list">
+            <ul class="list-unstyled d-flex flex-column gap-3">
+              <li>Order placement (3 orders/month)</li>
+              <li>Order tracking via tracking number</li>
+              <li>Order notifications (basic)</li>
+            </ul>
+          </div>
+        </div>
+        <div class="pricing-boottom">
+          <a href="#" class="btn btn-bgcolor btn-bgcolor-primary border-radius-8 color-white">Get Started</a>
+        </div>
+      </div>
+      <div class="pricing-card bg-white color-black d-flex flex-column">
+        <div class="pricing-top">
+          <div class="pricing-title">
+            <h4>Silver <a href="#">All Basic +</a></h4>
+            <p> No minimum commitment Pause or cancel anytime</p>
+          </div>
+          <div class="pricing-package">
+            <h3>200 NRS + VAT</h3>
+            <p> Per Month</p>
+            <div class="gradient-divider"></div>
+          </div>
+          <div class="pricing-list">
+            <ul class="list-unstyled d-flex flex-column gap-3">
+              <li>Unlimited order placement</li>
+              <li>Order scheduling</li>
+              <li>Template saving for repeat orders</li>
+              <li>Shipment Calendar</li>
+            </ul>
+          </div>
+        </div>
+        <div class="pricing-boottom">
+          <a href="#" class="btn btn-bgcolor btn-bgcolor-primary border-radius-8 color-white">Get Started</a>
+        </div>
+      </div>
+      <div class="pricing-card bg-white color-black d-flex flex-column">
+        <div class="pricing-top">
+          <div class="pricing-title">
+            <h4>Gold <a href="#">All Silver +</a></h4>
+            <p> No minimum commitment Pause or cancel anytime</p>
+          </div>
+          <div class="pricing-package">
+            <h3>300 NRS + VAT</h3>
+            <p> Per Month</p>
+            <div class="gradient-divider"></div>
+          </div>
+          <div class="pricing-list">
+            <ul class="list-unstyled d-flex flex-column gap-3">
+              <li>Detailed dispatch reporting</li>
+              <li>Real-time updates on tracking</li>
+              <li>Order cancellation</li>
+              <li>Payment term customization</li>
+            </ul>
+          </div>
+        </div>
+        <div class="pricing-boottom">
+          <a href="#" class="btn btn-bgcolor btn-bgcolor-primary border-radius-8 color-white">Get Started</a>
+        </div>
+      </div>
+    </div>
+
     <div class="row">
-      <div class="pricing-card bg-white color-black d-flex flex-column">
-          <div class="pricing-top">
+      <div class="col-lg-12 col-12">
+        <div class="d-flex flex-column bg-primary pricing-card pricing-card-diamond">
+          <div class="plan-info d-flex justify-content-between">
             <div class="pricing-title">
-              <h4>Basic</h4>
-              <p> No minimum commitment Pause or cancel anytime</p>
+              <h4>Diamond <a href="#">All Gold+</a></h4>
+              <p> No minimum commitment. Pause or cancel anytime</p>
             </div>
-            <div class="pricing-package">
-              <h3>Free</h3>
-              <p> Lifetime</p>
-              <div class="gradient-divider"></div>
-            </div>
-            <div class="pricing-list">
-              <ul class="list-unstyled d-flex flex-column gap-3">
-                <li>Order placement (3 orders/month)</li>
-                <li>Order tracking via tracking number</li>
-                <li>Order notifications (basic)</li>
-              </ul>
-            </div>
-          </div>
-          <div class="pricing-boottom">
-            <a href="#" class="btn btn-bgcolor btn-bgcolor-primary border-radius-8 color-white">Get Started</a>
-          </div>
-      </div>
-      <div class="pricing-card bg-white color-black d-flex flex-column">
-          <div class="pricing-top">
-            <div class="pricing-title">
-              <h4>Silver <a href="#">All Basic +</a></h4>
-              <p> No minimum commitment Pause or cancel anytime</p>
-            </div>
-            <div class="pricing-package">
-              <h3>200 NRS + VAT</h3>
+            <div class="pricing-package text-end">
+              <h3>500 NRS + VAT</h3>
               <p> Per Month</p>
-              <div class="gradient-divider"></div>
-            </div>
-            <div class="pricing-list">
-              <ul class="list-unstyled d-flex flex-column gap-3">
-                <li>Unlimited order placement</li>
-                <li>Order scheduling</li>
-                <li>Template saving for repeat orders</li>
-                <li>Shipment Calendar</li>
-              </ul>
+
             </div>
           </div>
-          <div class="pricing-boottom">
-            <a href="#" class="btn btn-bgcolor btn-bgcolor-primary border-radius-8 color-white">Get Started</a>
+
+          <div class="pricing-list d-flex justify-content-between align-items-end">
+            <ul class="list-unstyled d-grid gap-3">
+              <li>Predictive analytics for demand location</li>
+              <li>Vehicle usage trends</li>
+              <li>Advanced reporting tools </li>
+              <li>Privileged bidding for priority assignments</li>
+            </ul>
+            <a href="#" class="btn btn-bgcolor bg-black border-radius-8 color-white">Get Started</a>
           </div>
-      </div>
-      <div class="pricing-card bg-white color-black d-flex flex-column">
-          <div class="pricing-top">
-            <div class="pricing-title">
-              <h4>Gold <a href="#">All Silver +</a></h4>
-              <p> No minimum commitment Pause or cancel anytime</p>
-            </div>
-            <div class="pricing-package">
-              <h3>300 NRS + VAT</h3>
-              <p> Per Month</p>
-              <div class="gradient-divider"></div>
-            </div>
-            <div class="pricing-list">
-              <ul class="list-unstyled d-flex flex-column gap-3">
-                <li>Detailed dispatch reporting</li>
-                <li>Real-time updates on tracking</li>
-                <li>Order cancellation</li>
-                <li>Payment term customization</li>
-              </ul>
-            </div>
-          </div>
-          <div class="pricing-boottom">
-            <a href="#" class="btn btn-bgcolor btn-bgcolor-primary border-radius-8 color-white">Get Started</a>
-          </div>
+
+
+        </div>
+
       </div>
     </div>
 </section>
