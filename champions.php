161fa7e2f80bb31cfa543c02313f3eb38f1fe283
dewebkiller <dewebkiller@gmail.com>
2025-10-24 21:08:50 +0545

    championship page

---
+++ champions.php
@@ -0,0 +1,167 @@
+<?php include_once('header.php'); ?>
+<section class="content-wrapper section-padding section-hero-inner">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 col-md-7 col-12 hero-content-wrapper-inner">
+                <div class="hero-content">
+                    <h1>Drive, Deliver, and Win</h1>
+                    <p>Lodbod Champions is our loyalty program built for truck drivers and owners. The more trips you complete with Lodbod, the more rewards you unlock — from cash prizes to lucky draws and exclusive perks.</p>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<section class="section-padding section-how-it-works bg-light">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-12">
+                <?php include_once('sections/form.php'); ?>
+
+            </div>
+            <div class="col-lg-6 col-12 d-flex gap-3 flex-column">
+                <div class="d-flex flex-column">
+                    <h2>Lodbod Champions onboarding</h2>
+                    <p>Lodbod Champions is our loyalty program built for truck drivers and owners. The more trips you complete with Lodbod, the more rewards you unlock — from cash prizes to lucky draws and exclusive perks.</p>
+                </div>
+
+                <div class="d-flex flex-column">
+                    <h3> Provide the Essentials</h3>
+                    <p>We'll need some basic information to set up your Lodbod Champions status. This includes contact details and any relevant documents.</p>
+                </div>
+
+                <div class="d-flex row">
+                    <div class="col-lg-6 col-12">
+                        <p class="fw-bold">Requirements</p>
+                        <ul class="list-unstyled d-flex flex-column gap-1">
+                            <li><i class="fa-solid fa-circle-check color-primary"></i> Must 18 or older.</li>
+                            <li><i class="fa-solid fa-circle-check color-primary"></i> Must have vehicle documents.</li>
+                            <li><i class="fa-solid fa-circle-check color-primary"></i> Must have a valid license.</li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6 col-12">
+                        <p class="fw-bold">How to Sign Up</p>
+                        <ul class="list-unstyled d-flex flex-column gap-1">
+                            <li><i class="fa-solid fa-circle-check color-primary"></i> Fill out the application form.</li>
+                            <li><i class="fa-solid fa-circle-check color-primary"></i> Complete quick orientation.</li>
+                            <li><i class="fa-solid fa-circle-check color-primary"></i> Download the app and start driving.</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-padding-top">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-7 col-12">
+                <div class="d-flex flex-column bg-white p-4 rounded-3">
+                    <h2> How It Works:<br /> Delivering and Earning</h2>
+                    <p>TBecoming a Lodbod Champion is easy. Just follow these steps to start delivering and earning with us.</p>
+                    <ul class="list-unstyled steps-list steps-list-primary d-flex flex-column gap-3">
+                        <li class="step-item">
+                            <a href="#" class="step-link active-step">
+                                <h3 class="step-title">1. Register online</h3>
+                                <p class="step-description">Fill out the online form with your details and necessary documents.</p>
+                            </a>
+                        </li>
+
+                        <li class="step-item">
+                            <a href="#" class="step-link">
+                                <h3 class="step-title">2. Deliver order</h3>
+                                <p class="step-description">Receive delivery requests and ensure timely fulfillment.</p>
+                            </a>
+                        </li>
+
+                        <li class="step-item">
+                            <a href="#" class="step-link">
+                                <h3 class="step-title">3. Earn Money</h3>
+                                <p class="step-description">Earn for every successful delivery and win exciting prizes in lucky draw.</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-5 col-12">
+                <div class="mission-image">
+                    <img src="images/cta-image-sm.png" alt="How It Works" class="img-fluid" />
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="content-wrapper section-padding-4 bg-primary">
+    <div class="container">
+        <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+                <div class="hero-image float-end">
+                    <img src="images/championship.png" alt="Champion Ship" class="img-fluid">
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-12 hero-content-wrapper">
+                <div class="hero-content color-white">
+                    <h2>Lodbod Champions FAQs</h2>
+                    <p>Explore answers to common questions about joining Lodbod Champions program. If you need further assistance, feel free to contact us.</p>
+                </div>
+                <div class="championship-content-btm">
+                <div class="accordion d-flex flex-column gap-3" id="championfaq">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Question 1
+              </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#championfaq">
+              <div class="accordion-body">
+                <p><strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.</p>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Question 2
+              </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#championfaq">
+              <div class="accordion-body">
+                <p><strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.</p>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Question 3
+              </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#championfaq">
+              <div class="accordion-body">
+                <p><strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.</p>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingFour">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                Question 4
+              </button>
+            </h2>
+            <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#championfaq">
+              <div class="accordion-body">
+                <p><strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<?php include_once('footer.php');
--- sections/contact.php
+++ sections/contact.php
@@ -8,34 +8,7 @@
         </div>
         <div class="row">
             <div class="col-lg-6 col-12 offset-lg-3">
-
-                <div class="form-wrapper bg-white">
-                    <form action="#" method="post" class="contact-form">
-                        <ul class="list-unstyled d-flex flex-column gap-3">
-                            <li><input type="text" name="name" placeholder="Name" required class="form-control" /></li>
-
-                            <li><input type="email" name="email" placeholder="Email" required class="form-control" /></li>
-
-                            <li><input type="text" name="phone" placeholder="Phone" required class="form-control" /></li>
-                            <li>
-                                <textarea name="message" rows="4" placeholder="Message" required class="form-control"></textarea>
-                            </li>
-                            <li>
-                            <input type="checkbox" class="form-check-input" id="checkTerms" required>
-        <label class="form-check-label" for="checkTerms">
-            I agree to <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a>.
-        </label>
-                            </li>
-                            <li>
-                            <button type="submit" class="btn btn-bgcolor btn-bgcolor-primary border-radius-8 color-white">Send Message</button>
-                            </li>
-                        </ul>
-
-
-
-                        
-                    </form>
-                </div>
+                <?php include_once('form.php');?>
             </div>
         </div>
     </div>
